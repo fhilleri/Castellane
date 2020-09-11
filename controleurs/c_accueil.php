44d4e180455d77dea4a2e6a619767be181a9c313
HILLERITEAU Frédéric <hilleriteauf@sio-sm-49.local>
2020-09-11 10:25:04 +0200

    ajout police

--- controleurs/c_accueil.php
+++ controleurs/c_accueil.php
@@ -3,24 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Accueil</title>
 </head>
 <body>
     <header>
         <div class="navbar">
+            <a class="navbar_element" href="">Accueil</a>
             <a class="navbar_element" href="">Leçons</a>
             <a class="navbar_element" href="">Voitures</a>
-            <a class="navbar_element" href="">Accueil</a>
             <a class="navbar_element" href="">Moniteurs</a>
-            <a class="navbar_element" href="">Chronologie</a>
         </div>
     </header>
 
     <main>
-        <div class="main_content">
-
-        </div>
+        <h1>Leçons prévues</h1>
     </main>
 
 </body>
