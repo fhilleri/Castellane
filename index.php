3927188de0ef68a3d0924585215ef990ec4239a6
Kaeko252 <cuvierclement@yahoo.fr>
2020-09-11 08:45:55 +0200

    Creation index + class.pdo

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+require_once("modele/class.pdoCastellane.inc.php");
+
+if(!isset($_REQUEST['uc']))
+    $uc = 'accueil';
+else
+	$uc = $_REQUEST['uc'];
+
+$pdo = PdoCastellane::getPdoCastellane();	 
+switch($uc)
+{
+	case 'accueil':
+		{include("controleurs/c_voirClients.php");break;}
+	case 'creerLeçon' :
+		{include("controleurs/c_creationClient.php");break;}
+	case 'modifierLeçon' :
+		{include("controleurs/c_modifierClient.php");break;}
+	case 'supprimerLeçon' :
+        {include("controleurs/c_supprimerClient.php");break;}
+    case 'creerMoniteur' :
+        {include("controleurs/c_creationClient.php");break;}
+    case 'modifierMoniteur' :
+        {include("controleurs/c_modifierClient.php");break;}
+    case 'supprimerMoniteur' :
+        {include("controleurs/c_supprimerClient.php");break;}
+    case 'creerVoiture' :
+        {include("controleurs/c_creationClient.php");break;}
+    case 'modifierVoiture' :
+        {include("controleurs/c_modifierClient.php");break;}
+    case 'supprimerVoiture' :
+        {include("controleurs/c_supprimerClient.php");break;}
+}
+?>
