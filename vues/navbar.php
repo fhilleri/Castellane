5760c318c1522216e4062448fcf6b20fcd11352a
Nicolas <bosseaun@saintemarie-cholet.eu>
2020-09-25 10:04:33 +0200

    oui

--- vues/navbar.php
+++
@@ -1,9 +0,0 @@
-<header>
-    <div class="navbar">
-        <a class="navbar_element" href="index.php?uc=accueil">Accueil</a>
-        <a class="navbar_element" href="index.php?uc=afficherLecon">Leçons</a>
-        <a class="navbar_element" href="index.php?uc=afficherVoiture">Voitures</a>
-        <a class="navbar_element" href="index.php?uc=afficherMoniteur">Moniteurs</a>
-        <a class="navbar_element" href="index.php?uc=afficherClient">Clients</a>
-    </div>
-</header>
